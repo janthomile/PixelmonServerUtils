78e71f67995f26b8ec80a53f700ed3c763e11c5a
janthomile_beta <99990264+janthomile@users.noreply.github.com>
2025-01-04 00:44:29 -0500

    make spectate remap override possibly work with permission nodes

--- src/main/java/supermemnon/pixelmonutils/command/SpectateOverride.java
+++ src/main/java/supermemnon/pixelmonutils/command/SpectateOverride.java
@@ -7,6 +7,7 @@ import com.pixelmonmod.pixelmon.command.impl.SpectateCommand;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.CommandSource;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.server.permission.PermissionAPI;
 
@@ -14,6 +15,19 @@ public class SpectateOverride extends SpectateCommand {
     public SpectateOverride(CommandDispatcher<CommandSource> dispatcher) {
         super(dispatcher);
     }
+
+    @Override
+    public void execute(CommandSource sender, String[] args) throws CommandException {
+        try {
+            if (PermissionAPI.hasPermission(sender.getPlayerOrException(), PixelmonUtilsCommand.permissionSpectate)) {
+                sender = sender.withPermission(2);
+            }
+            super.execute(sender, args);
+        } catch (CommandSyntaxException e) {
+            sender.sendFailure(new StringTextComponent("There was an error executing the command."));
+        }
+    }
+
     public String getName() {
         return "spectatebattle";
     }
